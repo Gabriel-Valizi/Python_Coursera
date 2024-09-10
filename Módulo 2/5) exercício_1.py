1dc79947a29abe6cc3ba0c3d7abcc67c2121bad6
Valizii <gabrielvalizi500@gmail.com>
2024-09-10 15:16:05 -0300

    Módulo 2

--- Módulo 2/5) exercício_1.py
+++ Módulo 2/5) exercício_1.py
@@ -3,4 +3,4 @@ lado =  int(input("Digite o valor correspondente ao lado de um quadrado: "))
 perimetro = 4*lado
 area = lado **2
 
-print("perímetro: ", perimetro, " - área: ", area)
+print("perímetro:", perimetro, "- área:", area)
