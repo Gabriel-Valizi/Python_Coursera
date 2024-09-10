6ba3ea1ee21bd0fb49f199301fa58e3a2de8cb92
Valizii <gabrielvalizi500@gmail.com>
2024-09-10 14:50:54 -0300

    Módulo 2

---
+++ Módulo 2/modulo_2_entrada_de_dados.py
@@ -0,0 +1,7 @@
+tempF= input("Digite uma temperatura em fahrenheit: ")
+tempC = (float(tempF) - 32) * 5 /9
+
+print("A temperatura, em celsius, é ", tempC)
+
+# função input sempre devolve uma string
+# deve-se converter para int ou float
