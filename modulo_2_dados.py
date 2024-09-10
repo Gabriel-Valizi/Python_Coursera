0d70bc5065623faf24f8d3323b12a62722007b58
Valizii <gabrielvalizi500@gmail.com>
2024-09-10 14:29:19 -0300

    Módulo 2 - Dados

---
+++ modulo_2_dados.py
@@ -0,0 +1,23 @@
+print(10)
+print(type("oiiiiiiiii"))
+
+print(5/2)
+print(10/3)
+print(10//3)
+print(10%3)
+
+peso = 100
+altura=1.83
+
+IMC= peso/(altura**2)
+print(IMC)
+
+IMCint = int(IMC)
+print(IMCint)
+
+texto = "oiii"
+len(texto)
+print(len(texto))
+
+IMCstr = str(IMC)
+print(len(IMCstr))
