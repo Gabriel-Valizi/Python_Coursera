16860f2f699869a18168565432afcd1548b2affc
Valizii <gabrielvalizi500@gmail.com>
2024-09-10 15:11:29 -0300

    Módulo 2

--- Módulo 2/4) outros_inputs.py
+++ Módulo 2/4) outros_inputs.py
@@ -1,8 +1,8 @@
 segs_str = input("Por favor, entre com o número de segundos que deseja converter: ")
 total_segs = int(segs_str)
 
-horas = total_segs // 3600
-segs_restantes = total_segs % 3600
+horas = total_segs // 3600 # divisão inteira
+segs_restantes = total_segs % 3600 # resto da divisão
 
 minutos = segs_restantes // 60
 segs_restantes_final = segs_restantes % 60
