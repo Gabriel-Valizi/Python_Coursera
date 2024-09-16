035dc5e05e28f4b72e58a6acefbf78eb4a20f679
Valizii <gabrielvalizi500@gmail.com>
2024-09-16 15:20:52 -0300

    Módulo 4

---
+++ Módulo 4/5_exercício_2_extra.py
@@ -0,0 +1,17 @@
+n = int(input("Digite um número inteiro: "))
+ultimo_digito = n % 10
+n = n // 10
+adjacente_igual = False
+
+while n > 0:
+    proximo_digito = n % 10
+    if proximo_digito == ultimo_digito:
+        adjacente_igual = True
+        break
+    ultimo_digito = proximo_digito
+    n = n // 10
+
+if adjacente_igual:
+    print("sim")
+else:
+    print("não")
