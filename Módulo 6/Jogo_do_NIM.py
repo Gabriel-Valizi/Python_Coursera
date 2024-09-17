38ec28541134752b792c8e2821298377f0a8fa19
Valizii <gabrielvalizi500@gmail.com>
2024-09-17 16:07:24 -0300

    Módulo 6

--- Módulo 6/Jogo_do_NIM.py
+++ Módulo 6/Jogo_do_NIM.py
@@ -1,15 +1,85 @@
-#def computador_escolhe_jogada (n, m):
+#def campeonato ():
 
-    #return inteiro
+    #chamar partida 3 vezes
+
+##############################################################################
+
+def computador_escolhe_jogada (n, m):
+
+    # COMPUTADOR COMEÇANDO............
     
-#def usuario_escolhe_jogada (n, m):
+    tiradas = 0
+    deixadas = n - tiradas
+    resto = deixadas % (m+1)
 
-    #return inteiro
+    return tiradas
+
+    # USUARIO COMEÇANDO............
+
+    tiradas = 0
+    deixadas = n - tiradas
+    resto = deixadas % (m+1)
+
+    return tiradas
+    
+##############################################################################
+
+def usuario_escolhe_jogada (n, m):
+
+    tiradas = int(input("Quantas peças você vai tirar?"))
+
+    if tiradas > m:
+
+        print("Oops! Jogada inválida! Tente de novo.")
+
+    else: 
+
+        print("Você tirou", tiradas, "peças.")
+        deixadas = n - tiradas
+        print("Agora restam apenas", deixadas, "peças no tabuleiro")
+
+    return tiradas
+    
+################################################################################
 
 def partida ():
 
-    print("Bem-vindo ao jogo do NIM! Escolha:")
-    print("\n")
-    print("1 - para jogar uma partida isolada\n2 - para jogar um campeonato 2")
+    i = 1
+    fim_de_jogo = False
 
-partida()
+    while not fim_de_jogo:
+
+        print("**** Rodada", i, "****")
+
+        n = int(input("Quantas peças?"))
+        m = int(input("Limite de peças?"))
+
+        resto = n % (m+1)
+
+        # são múltiplos
+        if resto == 0:
+
+            print("Você começa!")
+            computador_escolhe_jogada (n, m)
+
+        else:
+
+            print("Computador começa!")
+            usuario_escolhe_jogada (n, m)
+
+################################################################################
+
+print("Bem-vindo ao jogo do NIM! Escolha:")
+print("1 - para jogar uma partida isolada\n2 - para jogar um campeonato 2")
+
+tipo_de_jogo = int(input("Escolha:"))
+
+if tipo_de_jogo == 1:
+
+    print("Você escolheu uma partida isolada!")
+    partida ()
+
+elif tipo_de_jogo == 2:
+
+    print("Você escolheu um campeonato!")
+    #campeonato()
