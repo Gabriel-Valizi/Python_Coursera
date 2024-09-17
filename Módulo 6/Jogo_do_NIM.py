1fa3dfa99a2636e21a088774ca0abae5dd8fb940
Valizii <gabrielvalizi500@gmail.com>
2024-09-17 16:05:26 -0300

    Módulo 6

---
+++ Módulo 6/Jogo_do_NIM.py
@@ -0,0 +1,15 @@
+#def computador_escolhe_jogada (n, m):
+
+    #return inteiro
+    
+#def usuario_escolhe_jogada (n, m):
+
+    #return inteiro
+
+def partida ():
+
+    print("Bem-vindo ao jogo do NIM! Escolha:")
+    print("\n")
+    print("1 - para jogar uma partida isolada\n2 - para jogar um campeonato 2")
+
+partida()
